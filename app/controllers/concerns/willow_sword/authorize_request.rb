d95032477e6e903982349574280d688b01185242
Kirk Wang <k3wang@gmail.com>
2025-08-20 13:29:07 -0700

    🐛 Fix service document for certain users
    Previously, when a user that does not have accessible Admin Sets or
    User Collections, the service document would fall apart.  Now it will
    error out gracefully and return a response with a 403 (forbidden).

--- app/controllers/concerns/willow_sword/authorize_request.rb
+++ app/controllers/concerns/willow_sword/authorize_request.rb
@@ -6,9 +6,10 @@ module WillowSword
       @current_user = nil
       return true unless WillowSword.config.authorize_request
       api_key = @headers.fetch(:api_key, nil)
-      @current_user = User.find_by(api_key: @headers[:api_key]) unless api_key.blank?
-      unless @current_user.present?
-        unless api_key.blank?
+      @current_user = User.find_by(api_key: api_key) if api_key.present?
+
+      if @current_user.blank?
+        if api_key.present?
           message = "Not authorized. API key not found."
         else
           message = "Not authorized. API key not available."
